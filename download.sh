fdf24057aa7b74fdd68eab86bbb21082fdb402ec
chunchi <tcc3810@gmail.com>
2022-10-15 20:24:17 +0000

    upload

--- download.sh
+++ download.sh
@@ -1,4 +1,5 @@
 if [ ! -f glove.840B.300d.txt ]; then
-  wget https://www.dropbox.com/s/jyp6yohixsyrnal/glove.840B.300d.zip?dl=1
-  unzip glove.840B.300d.zip?dl=1
+  wget https://www.dropbox.com/s/ugj33d9is8msxyi/glove.840B.300d.zip?dl=1 -O glove.840B.300d.zip
+  
+  unzip glove.840B.300d.zip
 fi
